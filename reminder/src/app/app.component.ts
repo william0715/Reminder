b758799f20a19c9a3c1aaf42f9a68f88f26b1c02
DESKTOP-941RN6B\ebkim <cico200@hanmail.net>
2019-01-21 23:58:23 +0900

    firebase 연동

--- reminder/src/app/app.component.ts
+++ reminder/src/app/app.component.ts
@@ -4,6 +4,16 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyA_3yV38hJ9rtFUap5ZKRzDp9PgI4fT7Oc",
+  authDomain: "reminder-3d33f.firebaseapp.com",
+  databaseURL: "https://reminder-3d33f.firebaseio.com",
+  projectId: "reminder-3d33f",
+  storageBucket: "reminder-3d33f.appspot.com"
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -22,5 +32,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
